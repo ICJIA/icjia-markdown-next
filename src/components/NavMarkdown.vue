002c657479b8024a329dd7763d4d8911416ba1b9
cschweda <cschweda@gmail.com>
2019-01-24 09:02:04 -0600

    fix: fix style sheet switcher

--- src/components/NavMarkdown.vue
+++ src/components/NavMarkdown.vue
@@ -6,7 +6,7 @@
         color="grey lighten-2"
         style="padding-top: 5px; z-index: 100; position: fixed; top: 50px;"
       >
-        <div v-for="tool in tools" :key="tool.action" class="hidden-sm-and-down">
+        <div v-for="tool in config.tools" :key="tool.action" class="hidden-sm-and-down">
           <v-tooltip bottom>
             <v-btn small icon slot="activator" @click.prevent="getEntity(tool.action)">
               <v-icon size="16px">{{tool.icon}}</v-icon>
@@ -36,7 +36,7 @@
         style="padding-top: 5px; z-index: 100; position: fixed; top: 50px;"
       >
         <v-select
-          :items="items"
+          :items="config.stylesheets"
           label="Stylesheet"
           v-model="select"
           @change="loadStyleSheet"
@@ -70,164 +70,38 @@
         </v-tooltip>
       </v-toolbar>
     </v-flex>
-    <!-- <v-spacer></v-spacer> -->
-    <!-- <v-select
-          :items="items"
-          label="Stylesheet"
-          v-model="select"
-          @change="loadStyleSheet"
-          color="indigo"
-        ></v-select>
-
-        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        <v-tooltip bottom>
-          <v-btn
-            flat
-            small
-            color="blue darken-4"
-            dark
-            slot="activator"
-            @click.prevent="getEntity('showHtml')"
-            style="font-size: 11px"
-          >Show HTML
-            <v-icon dark right style="font-size: 18px">code</v-icon>
-          </v-btn>
-          <span>Show HTML</span>
-        </v-tooltip>
-        <v-tooltip bottom>
-          <v-btn
-            flat
-            small
-            color="blue darken-4"
-            dark
-            slot="activator"
-            @click.prevent="getEntity('saveHtml')"
-            style="font-size: 12px"
-          >Save As HTML
-            <v-icon dark right style="font-size: 18px">save_alt</v-icon>
-          </v-btn>
-          <span>Save As HTML</span>
-    </v-tooltip>-->
-    <!-- </v-toolbar>
-    </v-flex>-->
   </v-layout>
 </template>
 
 <script>
 import { EventBus } from "@/event-bus.js";
+import config from "@/config";
 export default {
   mounted() {
-    this.loadStyleSheet("blank.css");
+    this.loadStyleSheet("default.css");
   },
   methods: {
     getEntity(action) {
       EventBus.$emit("entityEvent", action);
     },
     loadStyleSheet(e) {
-      if (e === "blank.css") {
-        try {
-          document.querySelector(`link[href$="/css/github.css"]`).remove();
-        } catch (e) {
-          console.log(e);
-        }
-        let file = document.createElement("link");
-        file.rel = "stylesheet";
-        file.href = "/css/blank.css";
-        document.head.appendChild(file);
+      if (this.currentStyleSheet) {
+        document
+          .querySelector(`link[href$="/css/${this.currentStyleSheet}"]`)
+          .remove();
       }
-
-      if (e === "github.css") {
-        document.querySelector(`link[href$="/css/blank.css"]`).remove();
-        let file = document.createElement("link");
-        file.rel = "stylesheet";
-        file.href = "/css/github.css";
-        document.head.appendChild(file);
-      }
+      let file = document.createElement("link");
+      file.rel = "stylesheet";
+      file.href = `/css/${e}`;
+      document.head.appendChild(file);
       this.currentStyleSheet = e;
     }
   },
   data() {
     return {
-      currentStyleSheet: "blank.css",
-      items: [
-        { text: "No Stylesheet", value: "blank.css" },
-        { text: "Github", value: "github.css" }
-      ],
-      select: { text: "No Stylesheet", value: "blank.css" },
-      tools: [
-        { name: "header", action: "header", icon: "title", tooltip: "Header" },
-        { name: "bold", action: "bold", icon: "format_bold", tooltip: "Bold" },
-        {
-          name: "italicize",
-          action: "italicize",
-          icon: "format_italic",
-          tooltip: "Italicize"
-        },
-        {
-          name: "blockquote",
-          action: "blockquote",
-          icon: "format_quote",
-          tooltip: "Blockquote"
-        },
-        { name: "link", action: "link", icon: "link", tooltip: "Link" },
-        {
-          name: "image",
-          action: "image",
-          icon: "insert_photo",
-          tooltip: "Image"
-        },
-        {
-          name: "unorderedList",
-          action: "unorderedList",
-          icon: "format_list_bulleted",
-          tooltip: "Bulleted List"
-        },
-        {
-          name: "orderedList",
-          action: "orderedList",
-          icon: "format_list_numbered",
-          tooltip: "Numbered List"
-        },
-
-        {
-          name: "footnote",
-          action: "footnote",
-          icon: "short_text",
-          tooltip: "Footnote"
-        },
-        {
-          name: "table",
-          action: "table",
-          icon: "table_chart",
-          tooltip: "Table"
-        },
-        {
-          name: "loremipsum",
-          action: "loremipsum",
-          icon: "texture",
-          tooltip: "Lorem Ipsum paragraph"
-        },
-        {
-          name: "mdToClipboard",
-          action: "mdToClipboard",
-          icon: "assignment",
-          tooltip: "Copy Markdown to clipboard"
-        }
-      ],
-      renderTools: [
-        {
-          name: "showHtml",
-          action: "showHtml",
-          icon: "code",
-          tooltip: "Show HTML"
-        },
-        {
-          name: "saveHtml",
-          action: "saveHtml",
-          icon: "save_alt",
-          tooltip: "Save HTML"
-        }
-      ]
+      currentStyleSheet: null,
+      select: { text: "Default", value: "default.css" },
+      config
     };
   }
 };
--- src/config/index.js
+++ src/config/index.js
@@ -13,6 +13,86 @@ const config = {
 
     quotes: "“”‘’"
   },
+  stylesheets: [
+    { text: "Default", value: "default.css" },
+    { text: "Github", value: "github.css" },
+    { text: "ICJIA", value: "icjia.css" }
+  ],
+
+  tools: [
+    { name: "header", action: "header", icon: "title", tooltip: "Header" },
+    { name: "bold", action: "bold", icon: "format_bold", tooltip: "Bold" },
+    {
+      name: "italicize",
+      action: "italicize",
+      icon: "format_italic",
+      tooltip: "Italicize"
+    },
+    {
+      name: "blockquote",
+      action: "blockquote",
+      icon: "format_quote",
+      tooltip: "Blockquote"
+    },
+    { name: "link", action: "link", icon: "link", tooltip: "Link" },
+    {
+      name: "image",
+      action: "image",
+      icon: "insert_photo",
+      tooltip: "Image"
+    },
+    {
+      name: "unorderedList",
+      action: "unorderedList",
+      icon: "format_list_bulleted",
+      tooltip: "Bulleted List"
+    },
+    {
+      name: "orderedList",
+      action: "orderedList",
+      icon: "format_list_numbered",
+      tooltip: "Numbered List"
+    },
+
+    {
+      name: "footnote",
+      action: "footnote",
+      icon: "short_text",
+      tooltip: "Footnote"
+    },
+    {
+      name: "table",
+      action: "table",
+      icon: "table_chart",
+      tooltip: "Table"
+    },
+    {
+      name: "loremipsum",
+      action: "loremipsum",
+      icon: "texture",
+      tooltip: "Lorem Ipsum paragraph"
+    },
+    {
+      name: "mdToClipboard",
+      action: "mdToClipboard",
+      icon: "assignment",
+      tooltip: "Copy Markdown to clipboard"
+    }
+  ],
+  renderTools: [
+    {
+      name: "showHtml",
+      action: "showHtml",
+      icon: "code",
+      tooltip: "Show HTML"
+    },
+    {
+      name: "saveHtml",
+      action: "saveHtml",
+      icon: "save_alt",
+      tooltip: "Save HTML"
+    }
+  ],
   templates: {
     table: `\r\n| Tables        | Are           | Cool  |
 | ------------- |:-------------:| -----:|
