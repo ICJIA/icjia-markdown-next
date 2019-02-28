8376afa6a4787b0a8bd65c089c8a98bccff5e40d
cschweda <cschweda@gmail.com>
2019-02-28 13:57:09 -0600

    fix: adjust tooltip

--- src/config/index.js
+++ src/config/index.js
@@ -44,7 +44,7 @@ const config = {
     },
     showYaml: {
       text:
-        "Display YAML metadata.<br><br><strong>YAML</strong> (<strong>Y</strong>AML <strong>A</strong>in't <strong>M</strong>arkup <strong>L</strong>anguage) is a human friendly data serialization standard. YAML is frequently used at the beginning of a markdown file to insert metadata and other information.<br><br>If YAML metadata does not exist, this button is disabled."
+        "Display YAML metadata.<br><br><strong>YAML</strong> (<strong>Y</strong>AML <strong>A</strong>in't <strong>M</strong>arkup <strong>L</strong>anguage) is a human friendly data serialization standard.<br><br>YAML is frequently used at the beginning of a markdown file to insert document-specific metadata.<br><br>If no YAML exists, this button is disabled."
     },
     saveHtml: {
       text: "Save as HTML"
