7d4a97a51ebda074ada3c443c8f66c3088390872
cschweda <cschweda@gmail.com>
2019-03-18 15:02:50 -0500

    fix: add markdown renderer

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -11,9 +11,11 @@
     <div class="box source">
       <textarea id="editor-source"></textarea>
     </div>
-    <div class="box target">
-      Rendered HTML here
-    </div>
+    <div
+      class="box target markdown-body"
+      id="viewer-scroll"
+      v-html="target"
+    ></div>
 
     <div class="box preview" v-if="isRenderingHtml">
       html preview here
@@ -55,12 +57,12 @@ const md = require("markdown-it")(config.markdownItOptions)
   .use(require("markdown-it-container"))
   .use(require("markdown-it-highlightjs"));
 
-// const { BitlyClient } = require("bitly");
-// const bitly = new BitlyClient(process.env.VUE_APP_BITLY, {});
-// const FileSaver = require("file-saver");
-// const loremIpsum = require("lorem-ipsum");
-// /* Thanks to: https://github.com/musicbed/mirrormark/blob/master/src/js/mirrormark.js */
-// const beautify_html = require("js-beautify").html;
+const { BitlyClient } = require("bitly");
+const bitly = new BitlyClient(process.env.VUE_APP_BITLY, {});
+const FileSaver = require("file-saver");
+const loremIpsum = require("lorem-ipsum");
+/* Thanks to: https://github.com/musicbed/mirrormark/blob/master/src/js/mirrormark.js */
+const beautify_html = require("js-beautify").html;
 
 export default {
   components: {
@@ -70,6 +72,15 @@ export default {
   },
   mounted() {
     this.initializeEditor();
+    this.initializeEditorEvents();
+    this.setInitialText();
+    this.initializeComponentEventListeners();
+    /**
+     * Turn off autosave in dev mopde
+     */
+    if (process.env.NODE_ENV != "development") {
+      this.initializeAutoSave();
+    }
   },
   methods: {
     initializeEditor: function() {
@@ -77,6 +88,121 @@ export default {
         document.getElementById("editor-source"),
         config.codeMirrorOptionsMarkdown
       );
+    },
+    initializeEditorEvents: function() {
+      EventBus.$emit("updateWordCount", this.wordCount);
+      this.editorScroll = document.querySelector(".CodeMirror-scroll");
+      this.viewerScroll = document.querySelector("#viewer-scroll");
+      this.editorScroll.addEventListener("scroll", this.updateViewerScroll);
+      this.viewerScroll.addEventListener("scroll", this.updateEditorScroll);
+      this.editor.on("change", cm => {
+        this.target = md.render(cm.getValue());
+        this.line = cm.getCursor(true);
+        this.lintMarkdown(cm.getValue());
+        this.config.session.markdownInProgress = cm.getValue();
+        // cm.setGutterMarker(3, "breakpoints", makeMarker());
+        /**
+         * Check if YAML delimter ('---') is present. If not, clear YAML.
+         * This is necessary since deleting YAML delimiters doesn't delete previously rendered YAML from markdown-it-meta.
+         */
+        let yamlCheck = this.editor.doc.getLine(0);
+        if (yamlCheck === "---") {
+          this.yaml = md.meta;
+        } else {
+          this.yaml = {};
+        }
+      });
+      this.editor.on("optionChange", cm => {
+        //console.log("option changed");
+      });
+    },
+    setInitialText() {
+      /* Is there html -> markdown in config.session? */
+      if (Object.entries(this.config.session.convertedMarkdown).length != 0) {
+        this.editor.getDoc().setValue(this.config.session.convertedMarkdown);
+        this.lintMarkdown(this.editor.getValue());
+        this.config.session.convertedMarkdown = "";
+        return;
+      }
+      /* Is there markdown-in-progress in config.session? */
+      if (Object.entries(this.config.session.markdownInProgress).length != 0) {
+        this.editor.getDoc().setValue(this.config.session.markdownInProgress);
+        this.lintMarkdown(this.editor.getValue());
+        return;
+      }
+      /* If neither, then first check for localstorage key ... */
+      if (config.localStorageKey in localStorage) {
+        let markdown = localStorage.getItem(config.localStorageKey);
+        this.editor.getDoc().setValue(markdown);
+        this.lintMarkdown(this.editor.getValue());
+      } else {
+        /* ... and if no localstage key, insert generic welcome. */
+        let welcomeMarkdown = require(`@/snippets/welcome.md`);
+        this.editor.getDoc().setValue(welcomeMarkdown);
+        this.lintMarkdown(this.editor.getValue());
+      }
+    },
+    initializeComponentEventListeners() {
+      EventBus.$on("entityEvent", action => {
+        this.insertEntity(action);
+      });
+      EventBus.$on("loadMarkdown", content => {
+        this.editor.getDoc().setValue(content);
+        console.log("Load markdown event: ", content);
+      });
+      // ping
+      EventBus.$on("getMarkdown", () => {
+        // pong
+        EventBus.$emit("sendMarkdown", this.markdown);
+      });
+      EventBus.$on("getSnippet", snippet => {
+        this.insert(snippet + "\n");
+      });
+      EventBus.$on("scrollSync", isScrollSynced => {
+        this.isScrollSynced = isScrollSynced;
+        let status = this.isScrollSynced ? "ON" : "OFF";
+        EventBus.$emit("displayStatus", `Scroll sync turned ${status}`);
+      });
+      EventBus.$on("updateEditorConfig", ({ option, value }) => {
+        this.editor.setOption(option, value);
+      });
+      EventBus.$on("saveMarkdownToLocalStorage", () => {});
+    },
+    initializeAutoSave() {
+      window.setInterval(() => {
+        let saveTime = new Date();
+        localStorage.setItem(config.localStorageKey, this.markdown);
+        console.log("Autosaved to local storage: ", saveTime);
+      }, config.autoSaveInterval);
+    },
+    lintMarkdown(content) {
+      const options = {
+        strings: {
+          content
+        },
+        config: require(`@/${this.config.lintingRulePath}${
+          this.config.lintingDefault
+        }`)
+      };
+      window.markdownlint(options, (err, result) => {
+        let lintStatus = {};
+        if (!err) {
+          if (result.toString().length) {
+            console.error("Linting error");
+            console.log(result.toString());
+            lintStatus.isError = true;
+            lintStatus.result = result;
+          } else {
+            //console.log("No linting errors");
+            lintStatus.isError = false;
+            lintStatus.result = {};
+            lintStatus.result.content = "";
+          }
+          this.$nextTick(() => {
+            EventBus.$emit("lintStatus", lintStatus);
+          });
+        }
+      });
     }
   },
   computed: {
@@ -89,7 +215,8 @@ export default {
   },
   data() {
     return {
-      config: store.config
+      config: store.config,
+      target: ""
     };
   }
 };
@@ -106,6 +233,7 @@ export default {
 .target {
   grid-area: target;
   background: #fff;
+  padding: 10px;
 }
 
 .preview {
